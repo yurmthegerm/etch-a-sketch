4c1b4572b2f707d0819ea509ec324468fb5e9087
Yurim Kang <yoorim7480@gmail.com>
2023-02-16 11:07:30 -0500

    get number of grid size by user input

--- myscript.js
+++ myscript.js
@@ -5,9 +5,12 @@ function boxes() {
     if (num == null || num == "") {
         text = "Invalid";
     } else {
+        const measure = 960/num;
         for (i=0; i<(parseInt(num)**2); i++) {
             let child = document.createElement('div');
             child.className = 'child';
+            child.style.width = `${measure}px`;
+            child.style.height = `${measure}px`;
             document.getElementById('container').appendChild(child);
         
         };
