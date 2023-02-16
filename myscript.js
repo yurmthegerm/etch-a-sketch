3721cd36393eef9a79a300cd069300b6ee860373
Yurim Kang <yoorim7480@gmail.com>
2023-02-16 10:55:43 -0500

    add button that prompts a pop-up

--- myscript.js
+++ myscript.js
@@ -1,6 +1,24 @@
-for (i=0; i<16; i++) {
-    let child = document.createElement('div');
-    child.className = 'child';
-    document.getElementById('container').appendChild(child);
-    console.log(i);
+
+function boxes() {
+    let text;
+    let num = prompt("what by what?");
+    if (num == null || num == "") {
+        text = "Invalid";
+    } else {
+        for (i=0; i<(parseInt(num)**2); i++) {
+            let child = document.createElement('div');
+            child.className = 'child';
+            document.getElementById('container').appendChild(child);
+        
+        };
+    }
+}
+
+const children = document.getElementsByClassName('child');
+Array.from(children).forEach(changeColour);
+
+function changeColour(item) {
+    item.addEventListener('mouseover', function (){
+        item.style.backgroundColor = 'yellow';
+    });
 }
